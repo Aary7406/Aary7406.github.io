959bec89587868f3e4b386db6d21b477b62a7da3
Aary7406 <aary742006@gmail.com>
2025-05-06 02:39:38 -0700

    same as before

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Aary7406.github.io',
+  base: '/aary7406.github.io',
 })
